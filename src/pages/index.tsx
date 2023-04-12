5297043481be2b3fcf3859553ae859cf24a25118
sutra9 <jamarpowell.dev@gmail.com>
2023-04-12 18:37:37 -0400

    initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,76 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import data from '../../db/data.json'
+
+
+type Station = {
+  website_title: string,
+  website_description: string,
+  website_url: string,
+  website_image: string,
+  website_feed?: string
+}
+
+type Stations = Station[]
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ stations }: { stations: Stations }) {
+  console.log(stations)
+  const p = 'https://wsrv.nl/?url='
+
+  const nofeed = 'No Feed'
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className='p-8 container mx-auto'>
+      <h1 className='text-5xl mb-8'>SoundNav</h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <ul className='flex gap-12 md:gap-8 flex-wrap flex-col md:flex-row md:justify-between '>
+        {
+          stations && stations.map((station, index) => (
+            <li key={index}>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <section className='transition-all hover:shadow-2xl md:w-96 md:min-w-[20rem] h-full p-4 border border-black flex flex-col md:justify-between mb-2 '>
+                <div className=''>
+                  <div className="flex justify-between mb-3 items-center gap-3 border-b-2 pb-2 border-black">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+                    <h2 className='text-lg font-bold'>{station.website_title}</h2>
+                    {station.website_image.length > 0 && (
+                      <Image className='max-w-[32px] overflow-hidden object-contain rounded-lg' src={`${p + station.website_image}`} alt='icon' width={32} height={32} />
+                    )
+                    }
+                  </div>
+                  <p className='text-base text-gray-600 mb-2'>{station.website_description}</p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                </div>
+                <div className='flex justify-between items-center '>
+                  <a className='hover:bg-black border border-black hover:text-white p-2 bg-white text-black' target="_blank" rel="noopener noreferrer" href={`${station.website_url}`}>Source</a>
+
+                  {station.website_feed && station.website_feed.length > 0 ? (
+
+                    <a target="_blank" rel="noopener noreferrer" className='hover:bg-black border border-black hover:text-white p-2 bg-white text-black' href={`${station.website_feed}`}>Feed Available</a>
+                    // if user right clicks to download, it will download the feed and name it after the website title
+
+                  ) : (
+                    <p className='text-red-400 p-2 uppercase '>{nofeed}</p>
+                  )}
+
+                </div>
+              </section>
+
+            </li>
+          ))
+        }
+      </ul>
+
+    </main >
   )
+}
+
+
+export async function getStaticProps() {
+
+  return {
+    props: { stations: data }, // will be passed to the page component as props
+  }
 }
